d2d073f7459dbacf8230d1813cc61810d50bdaf4
Sebastiaan Besselsen <sebastiaan.besselsen@gmail.com>
2024-01-03 20:40:12 +0100

    Add perft for some standard positions as tests (speed is GREAT!)

--- src/bitboard.rs
+++ src/bitboard.rs
@@ -1397,6 +1397,20 @@ impl BitBoard {
             .nth(file as usize)
             .expect("file index should be valid")
     }
+
+    #[allow(dead_code)]
+    fn perft(&self, n: u32) -> u32 {
+        if n == 0 {
+            return 0;
+        }
+
+        let next_boards = self.next_boards();
+        if n == 1 {
+            next_boards.len() as u32
+        } else {
+            next_boards.into_iter().map(|b| b.perft(n - 1)).sum()
+        }
+    }
 }
 
 impl Default for BitBoard {
@@ -2084,5 +2098,60 @@ mod tests {
         board2.can_castle[COLOR_BLACK][SIDE_KING] = false;
         board2.compute_hash();
         assert_eq!(board2.zobrist_hash, incremental_hash);
+    }
+
+    #[test]
+    fn perft_low() {
+        let board = BitBoard::new_setup();
+
+        assert_eq!(board.perft(1), 20);
+        assert_eq!(board.perft(2), 400);
+        assert_eq!(board.perft(3), 8_902);
+        assert_eq!(board.perft(4), 197_281);
+    }
+
+    #[ignore]
+    #[test]
+    fn perft_high() {
+        let board = BitBoard::new_setup();
+
+        assert_eq!(board.perft(5), 4_865_609);
+        assert_eq!(board.perft(6), 119_060_324);
+    }
+
+    #[test]
+    fn perft_kiwipete() {
+        let board = BitBoard::try_parse_fen(
+            "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 0",
+        )
+        .unwrap();
+
+        assert_eq!(board.perft(3), 97_862);
+        assert_eq!(board.perft(4), 4_085_603);
+    }
+
+    #[test]
+    fn perft_position_5() {
+        // See https://www.chessprogramming.org/Perft_Results
+        let board =
+            BitBoard::try_parse_fen("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8")
+                .unwrap();
+
+        assert_eq!(board.perft(3), 62_379);
+        assert_eq!(board.perft(4), 2_103_487);
+    }
+
+    #[test]
+    fn perft_position_6() {
+        // See https://www.chessprogramming.org/Perft_Results
+        let board = BitBoard::try_parse_fen(
+            "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10",
+        )
+        .unwrap();
+
+        assert_eq!(board.perft(1), 46);
+        assert_eq!(board.perft(2), 2_079);
+        assert_eq!(board.perft(3), 89_890);
+        assert_eq!(board.perft(4), 3_894_594);
     }
 }
